8f95ca85267c04fd3099218cc0adecf2a7b3f9af
adrianopauli <adrianopauli03@gmail.com>
2013-11-14 22:48:57 -0200

    Codigo do receptor de IR

--- desenvolviemnto/servidor/auto/auto/settings.py
+++ desenvolviemnto/servidor/auto/auto/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'automacao_db',                      # Or path to database file if using sqlite3.
         'USER': 'automacao',                      # Not used with sqlite3.
         'PASSWORD': 'automacao',                  # Not used with sqlite3.
-        'HOST': '192.168.6.223',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '192.168.1.101',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                     # Set to empty string for default. Not used with sqlite3.
     }
 }
--- desenvolviemnto/servidor/auto/auto/urls.py
+++ desenvolviemnto/servidor/auto/auto/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^$','controles.views.index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^teste/$','controles.views.index'),
     url(r'^controles/$','controles.views.controles'),
     url(r'^ajax/ir/$','controles.ajax.sendIR'),
     url(r'^controle/(?P<controle_id>\d+)$','controles.views.controle'),
