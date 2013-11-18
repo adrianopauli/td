78f64d89fcb98289b0510686e96aed0fb6adecff
adrianopauli <adrianopauli03@gmail.com>
2013-11-18 12:31:57 -0200

    comunicacao

---
+++ desenvolviemnto/servidor/auto/controles/comunicacao.py
@@ -0,0 +1,16 @@
+#import serial
+#import RPi.GPIO as GPIO
+
+
+class rs485(object):
+	
+	#serial = serial.Serial('/dev/ttyAMAO',4800,timeout=1)
+	
+	#def __init__(self):
+		#serial.open()
+
+	def sendComand(self,comando):
+		#ser.write(comando)
+		return "ok"
+
+
